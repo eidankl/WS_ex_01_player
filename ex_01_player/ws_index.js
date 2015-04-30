fe559af95ebfe0d671e6f1f499d1c96881a428a5
Eidan Kleinamn <eidankl@gmail.com>
2015-05-01 00:00:12 +0300

    second commit

--- ex_01_player/ws_index.js
+++ ex_01_player/ws_index.js
@@ -3,10 +3,10 @@ var http = require ('http');
 var player = require ('./Player');
 
 http.createServer(function(req,res){
-	res.writeHead(200);	//sttus code in header -->200=OK
-	res.write("We build a server");	//response body
-	res.end();	//close connection
-}).listen(8080);							//listen for connection on this port
+	res.writeHead(200);					//sttus code in header -->200=OK
+	res.write(player.getMsg());			//response body
+	res.end();							//close connection
+}).listen(8080);						//listen for connection on this port
 console.log("listening on port 8080");
 
 //----create a Player instance and attach callbacks to event
@@ -16,6 +16,10 @@ player.on("changePoints", player.getPoints);
 
 //---run some methodes
 player.increment(5);
-player.decrement(2);
+player.decrement(4);
+player.increment(5);
+player.increment(5);
+player.increment(5);
 
+						
 						
